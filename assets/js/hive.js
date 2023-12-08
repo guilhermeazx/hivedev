64e6474348f5be88aca319e1b76396737ce58386
Guilherme Assis <guiassishg11@gmail.com>
2023-12-08 16:28:07 -0300

    creation of the modal

--- assets/js/hive.js
+++ assets/js/hive.js
@@ -17,3 +17,23 @@ $(document).ready(function(){
     });
 });
 // aqui
+// Adicione isso ao seu arquivo de script existente ou em um novo arquivo
+function openModal(imageSrc) {
+    var modal = document.getElementById('myModal');
+    var modalImage = document.getElementById('modalImage');
+    modal.style.display = 'block';
+    modalImage.src = imageSrc;
+}
+
+function closeModal() {
+    var modal = document.getElementById('myModal');
+    modal.style.display = 'none';
+}
+
+// Fechar o modal se clicar fora da imagem
+window.onclick = function (event) {
+    var modal = document.getElementById('myModal');
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
